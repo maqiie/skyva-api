a9dff4148c60ba25f3089675986505918f79ce8e
maqiie <99504010+maqiie@users.noreply.github.com>
2023-10-02 10:14:13 +0300

    updated the products controller

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,22 +1,27 @@
 # app/controllers/products_controller.rb
 
 class ProductsController < ApplicationController
+
   def create
     @product = Product.new(product_params)
-  
+
     if @product.save
+      # Handle image upload here
+      if params[:product][:image].present?
+        @product.image.attach(params[:product][:image])
+      end
+      
       render json: { message: 'Product was successfully created', product: @product }, status: :created
     else
       render json: { errors: @product.errors.full_messages }, status: :unprocessable_entity
     end
   end
-  
 
-  # Other actions...
 
   private
   def product_params
-    params.require(:product).permit(:name, :description, :price, :size,:brand, :stock_quantity, :color, :category_id)
+    params.require(:product).permit(:name, :description, :price, :size,:brand, :stock_quantity, :color, :category_id, :image)
   end
+
   
 end

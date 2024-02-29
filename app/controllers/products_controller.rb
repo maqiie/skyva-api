7762904ea1b9ce9f14259fb2b55968d6b3244750
maqiie <99504010+maqiie@users.noreply.github.com>
2024-02-29 22:45:24 +0300

    updated the backend

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -3,63 +3,40 @@
 class ProductsController < ApplicationController
   before_action :require_admin, only: [:create]
 
+ 
+  def destroy
+    @product = Product.find(params[:id])
 
-   # GET /products
-   def index
-    @products = Product.all
-    render json: @products
+    if @product.destroy
+      render json: { message: 'Product was successfully deleted' }, status: :ok
+    else
+      render json: { errors: @product.errors.full_messages }, status: :unprocessable_entity
+    end
   end
-  # def index
-  #   @products = Product.all
-    
-  #   respond_to do |format|
-  #     format.json do
-  #       # Create an array to store the JSON representation of each product
-  #       products_data = []
-  #       @products.each do |product|
-  #         product_data = product.as_json
-  #         if product.image.attached?
-  #           product_data["image_url"] = rails_blob_path(product.image, only_path: true)
-  #         end
-  #         products_data << product_data
-  #       end
-  #       render json: products_data
-  #     end
-  #   end
-  # end
+
+
+
+
+  def index
+    @products = Product.all
+    render json: @products.map { |product| product_with_image_url(product) }
+  end
   
 
+
   
-  # def create
-  #   @product = Product.new(product_params)
-    
-  #   # Set the on_offer attribute based on the incoming parameter
-  #   @product.on_offer = params[:product][:on_offer] if params[:product][:on_offer].present?
-  
-  #   if @product.save
-  #     # Handle image upload here
-  #     if params[:product][:image].present?
-  #       @product.image.attach(params[:product][:image])
-  #     end
-      
-  #     render json: { 
-  #       message: 'Product was successfully created', 
-  #       product: @product.as_json
-  #     }, status: :created
-  #   else
-  #     render json: { errors: @product.errors.full_messages }, status: :unprocessable_entity
-  #   end
-  # end
-  
+
   def create
     @product = Product.new(product_params)
-  
+    
     # Set the on_offer attribute based on the incoming parameter
     @product.on_offer = params[:product][:on_offer] if params[:product][:on_offer].present?
-  
+    
     if @product.save
-      # Handle image upload here
-      if params[:product][:image].present?
+      image_url = nil
+      
+      # Handle image upload here if the image parameter is present
+      if params[:product].present? && params[:product][:image].present?
         begin
           @product.image.attach(params[:product][:image])
           image_url = url_for(@product.image) # Get the URL of the attached image
@@ -80,18 +57,7 @@ class ProductsController < ApplicationController
   end
   
   
-  # app/controllers/products_controller.rb
 
-
-  # def on_offer
-  #   @products = Product.on_offer
-  #   render json: @products
-  # end
-  
-  # def recently_added
-  #   @products = Product.recently_added
-  #   render json: @products
-  # end
   def on_offer
     @products = Product.on_offer.with_attached_image
     render json: @products.map { |product| product_with_image_url(product) }
@@ -112,19 +78,6 @@ class ProductsController < ApplicationController
     product_data
   end
   
-
-
- 
-  def destroy
-    @product = Product.find(params[:id])
-
-    if @product.destroy
-      render json: { message: 'Product was successfully deleted' }, status: :ok
-    else
-      render json: { errors: @product.errors.full_messages }, status: :unprocessable_entity
-    end
-  end
-
 
 
 

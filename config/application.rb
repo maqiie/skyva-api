6298d7ad662d79d6b9d023e584392aa1c5e2787a
maqiie <markpaulmuthauragmail.com>
2024-04-03 12:05:41 +0300

    solving cors errors

--- config/application.rb
+++ config/application.rb
@@ -87,15 +87,25 @@ config.action_cable.mount_path = '/cable'
     config.middleware.use ActionDispatch::Flash
 
 
+    # config.middleware.insert_before 0, Rack::Cors do
+    #   allow do
+    #     origins 'https://skyva-front.vercel.app'
+    #     resource '*',
+    #              :headers => :any,
+    #              :expose => ['access-token', 'expiry', 'token-type', 'uid', 'client'],
+    #              :methods => [:get, :post, :options, :delete, :put, :patch]
+    #   end
+    # end
     config.middleware.insert_before 0, Rack::Cors do
       allow do
-        origins 'https://skyva-front.vercel.app'
+        origins 'https://skyva-front.vercel.app', 'https://skyva-admin.vercel.app'
         resource '*',
                  :headers => :any,
                  :expose => ['access-token', 'expiry', 'token-type', 'uid', 'client'],
                  :methods => [:get, :post, :options, :delete, :put, :patch]
       end
     end
+    
     # ここまで
   end
 end

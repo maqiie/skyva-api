f3b7543875ebd268be492cd60ba65ca729593f1c
maqiie <markpaulmuthauragmail.com>
2024-04-11 13:41:51 +0300

    cors errors2

--- config/application.rb
+++ config/application.rb
@@ -120,6 +120,64 @@
 # end
 
 
+# require_relative 'boot'
+
+# require "rails"
+# # Pick the frameworks you want:
+# require "active_model/railtie"
+# require "active_job/railtie"
+# require "active_record/railtie"
+# require "active_storage/engine"
+# require "action_controller/railtie"
+# require "action_mailer/railtie"
+# require "action_view/railtie"
+# require "action_cable/engine"
+# # require "sprockets/railtie"
+# # require "rails/test_unit/railtie"
+
+# # Require the gems listed in Gemfile, including any gems
+# # you've limited to :test, :development, or :production.
+# Bundler.require(*Rails.groups)
+
+# module DeviseTokenAuthTwitter
+#   class Application < Rails::Application
+#     # Initialize configuration defaults for originally generated Rails version.
+#     config.load_defaults 5.2
+
+#     # Settings in config/environments/* take precedence over those specified here.
+#     # Application configuration can go into files in config/initializers
+#     # -- all .rb files in that directory are automatically loaded after loading
+#     # the framework and any gems in your application.
+#     # config.middleware.use ActionDispatch::Session::CookieStore
+#     # Only loads a smaller set of middleware suitable for API only apps.
+#     # Middleware like session, flash, cookies can be added back manually.
+
+#     config.generators.system_tests = nil
+#     # Skip views, helpers and assets when generating a new resource.
+#     config.api_only = true
+
+#     # ここからコピペする
+#     config.session_store :cookie_store, key: '_interslice_session'
+#     config.middleware.use ActionDispatch::Cookies # Required for all session management
+#     config.middleware.use ActionDispatch::Session::CookieStore, config.session_options
+#     config.middleware.use ActionDispatch::Flash
+
+
+# # config/initializers/cors.rb
+# Rails.application.config.middleware.insert_before 0, Rack::Cors do
+#   allow do
+#     origins 'https://skyva-front.vercel.app'
+
+#     resource '/auth',
+#       headers: :any,
+#       methods: [:post],
+#       credentials: true
+#   end
+# end
+#     # ここまで
+#   end
+# end
+
 require_relative 'boot'
 
 require "rails"
@@ -156,34 +214,16 @@ module DeviseTokenAuthTwitter
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
 
-    # ここからコピペする
-    config.session_store :cookie_store, key: '_interslice_session'
-    config.middleware.use ActionDispatch::Cookies # Required for all session management
-    config.middleware.use ActionDispatch::Session::CookieStore, config.session_options
-    config.middleware.use ActionDispatch::Flash
-
-
-    # config.middleware.insert_before 0, Rack::Cors do
-    #   allow do
-    #     origins '*'
-    #     resource '*',
-    #              :headers => :any,
-    #              :expose => ['access-token', 'expiry', 'token-type', 'uid', 'client'],
-    #              :methods => [:get, :post, :options, :delete, :put]
-    #   end
-    # end
+    # Set up CORS configuration
     config.middleware.insert_before 0, Rack::Cors do
       allow do
-        # origins 'https://ibl0g.vercel.app' # Replace with your client
-                origins 'https://skyva-front.vercel.app', 'https://skyva-admin.vercel.app', 'http://localhost:3000'
+        origins 'https://skyva-front.vercel.app'
 
-        resource '*',
+        resource '/auth',
           headers: :any,
-          expose: ['access-token', 'expiry', 'token-type', 'uid', 'client'],
-          methods: [:get, :post, :options, :delete, :put, :patch]
+          methods: [:post],
+          credentials: true
       end
     end
-    
-    # ここまで
   end
 end

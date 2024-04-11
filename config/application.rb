656c00d9a4cffaca967aaf34b01ac5a9e285d423
maqiie <markpaulmuthauragmail.com>
2024-04-11 16:20:50 +0300

    cors

--- config/application.rb
+++ config/application.rb
@@ -214,16 +214,32 @@ module DeviseTokenAuthTwitter
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
 
-    # Set up CORS configuration
+    # ここからコピペする
+    config.session_store :cookie_store, key: '_interslice_session'
+    config.middleware.use ActionDispatch::Cookies # Required for all session management
+    config.middleware.use ActionDispatch::Session::CookieStore, config.session_options
+    config.middleware.use ActionDispatch::Flash
+
+
+    # config.middleware.insert_before 0, Rack::Cors do
+    #   allow do
+    #     origins '*'
+    #     resource '*',
+    #              :headers => :any,
+    #              :expose => ['access-token', 'expiry', 'token-type', 'uid', 'client'],
+    #              :methods => [:get, :post, :options, :delete, :put]
+    #   end
+    # end
     config.middleware.insert_before 0, Rack::Cors do
       allow do
-        origins 'https://skyva-front.vercel.app'
-
-        resource '/auth',
+        origins 'https://skyva-front.vercel.app/','https://skyva-admin.vercel.app/'
+        resource '*',
           headers: :any,
-          methods: [:post],
-          credentials: true
+          expose: ['access-token', 'expiry', 'token-type', 'uid', 'client'],
+          methods: [:get, :post, :options, :delete, :put]
       end
     end
+    
+    # ここまで
   end
 end

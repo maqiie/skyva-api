9471a4982725c02a37ed7881f50cfea101709ac8
maqiie <markpaulmuthauragmail.com>
2024-04-11 16:55:32 +0300

    migrations

--- db/migrate/20230928093423_create_products.rb
+++ db/migrate/20230928093423_create_products.rb
@@ -11,6 +11,10 @@ class CreateProducts < ActiveRecord::Migration[7.0]
       t.references :category, null: false, foreign_key: true
 
       t.timestamps
+      
+
     end
+
+    depends_on :categories
   end
 end

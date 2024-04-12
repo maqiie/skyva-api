9d3328ccddda548c542eb888d58076c42d73aac2
maqiie <markpaulmuthauragmail.com>
2024-04-12 17:54:10 +0300

    added patch to the cors

--- config/application.rb
+++ config/application.rb
@@ -224,7 +224,7 @@ module DeviseTokenAuthTwitter
         resource '*',
           headers: :any,
           expose: ['access-token', 'expiry', 'token-type', 'uid', 'client'],
-          methods: [:get, :post, :options, :delete, :put]
+          methods: [:get, :post, :options, :delete, :put, :patch]
       end
     end
   end

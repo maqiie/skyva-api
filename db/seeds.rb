5003fa9629c3fa92e3d6bc3b868dad93fb196a0d
maqiie <markpaulmuthauragmail.com>
2024-04-22 16:19:20 +0300

    added the netlify frontend to te cors

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,18 +1,18 @@
-# # db/seeds.rb
-# require 'faker'
+# db/seeds.rb
+require 'faker'
 
-# # Create random categories
+# Create random categories
 
-# # Create specific categories
-# Category.create(name: 'Clothes', description: 'Clothing category')
-# Category.create(name: 'Shoes', description: 'Footwear category')
-# Category.create(name: 'Jewelry', description: 'Jewelry category')
-# Category.create(name: 'Cosmetics', description: 'cosmetics\'s category')
-# Category.create(name: 'Women', description: 'Women\'s clothing')
+# Create specific categories
+Category.create(name: 'Clothes', description: 'Clothing category')
+Category.create(name: 'Shoes', description: 'Footwear category')
+Category.create(name: 'Jewelry', description: 'Jewelry category')
+Category.create(name: 'Cosmetics', description: 'cosmetics\'s category')
+Category.create(name: 'Women', description: 'Women\'s clothing')
 
-# # Create random brands
-# 5.times do
-#   Brand.create(name: Faker::Company.name, description: Faker::Lorem.sentence)
-# end
+# Create random brands
+5.times do
+  Brand.create(name: Faker::Company.name, description: Faker::Lorem.sentence)
+end
 
-# puts 'Seed data has been created.'
+puts 'Seed data has been created.'
